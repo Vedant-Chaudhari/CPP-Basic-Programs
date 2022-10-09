a621e11bbc3f8fa8270464287521f9ad4f1fa8ec
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-10 01:10:58 +0530

    Add files via upload

---
+++ Enum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+  enum meal{Breakfast, Lunch, Dinner};
+  cout << Breakfast << endl ;
+  cout << Lunch << endl ;
+  cout << Dinner << endl ;
+
+  return 0 ;
+  
+}

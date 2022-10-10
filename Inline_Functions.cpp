ddea804aeae4ed8d59ecad94e9677851458e0a94
Vedant Chaudhari <114819383+vedant237@users.noreply.github.com>
2022-10-10 12:13:26 +0530

    Add files via upload

---
+++ Inline_Functions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std ;
+
+inline int product(int a, int b)
+{
+  return a*b ;
+}
+
+int main()
+{
+  int a, b ;
+  cout <<"Enter the First Number : " ;
+  cin >> a ;
+  cout <<"Enter the Second Number : " ;
+  cin >> b ;
+
+  cout <<"The Product of First and Second Number is : "<< product(a,b) <<endl ;
+  cout <<"The Product of First and Second Number is : "<< product(a,b) <<endl ;
+  cout <<"The Product of First and Second Number is : "<< product(a,b) <<endl ;
+  cout <<"The Product of First and Second Number is : "<< product(a,b) <<endl ;
+  cout <<"The Product of First and Second Number is : "<< product(a,b) <<endl ;
+  cout <<"The Product of First and Second Number is : "<< product(a,b) <<endl ;
+  cout <<"The Product of First and Second Number is : "<< product(a,b) <<endl ;
+  cout <<"The Product of First and Second Number is : "<< product(a,b) <<endl ;
+  cout <<"The Product of First and Second Number is : "<< product(a,b) <<endl ;
+
+  return 0 ;
+}
